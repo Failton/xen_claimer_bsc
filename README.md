0c88fa78473b9f89295d31d6d405e7d1889f46d9
Failton <ilya.zig.good@gmail.com>
2022-10-12 23:24:18 +0500

    add config.txt and update README.md

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # xen_claimer_bsc
 
-Софт на xen.network (https://xen.network)
+Софт на xen.network(https://xen.network) 
 
 Сбор токенов с фермы кошелей и отправка на основной адрес (одной транзакцией)
 
-Указать основной адрес в файле main.py в 6 строке (your_account = ' ')
+Основной адрес поместить в файл config.txt
 
-Кошели поместить в файл accounts.txt построчно, в формате address:private_key:чтоугодноеще
+Кошели поместить в файл accounts.txt построчно, в формате address:private_key
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from loguru import logger
 from sys import stderr
 from threading import Thread
 
-your_account = '' # НАПИШИ СВОЙ АДРЕС ЗДЕСЬ
-gas_price = 7
+gas_price = 7 # ОТРЕДАЧЬ GAZ PRICE ЕСЛИ ХОЧЕШЬ
 
 # SETTINGS
 file_accounts = 'files/accounts.txt'
 file_abi = 'files/abi.txt'
+file_config = 'config.txt'
 web3_http_provider = 'https://bsc-dataseed.binance.org/'
 contract_address = '0x2AB0e9e4eE70FFf1fB9D67031E44F6410170d00e'
 
@@ -22,14 +22,14 @@ def parse_accounts(file):
         accounts = [row.strip().split(':') for row in file]
     return accounts
 
-def claim_mint_reward_and_share(account, contract):
+def claim_mint_reward_and_share(account, contract, main_account):
     address = web3.toChecksumAddress(account[0])
     private_key = account[1]
 
     try:
 
         transaction = contract.functions.claimMintRewardAndShare(
-            your_account, 100).buildTransaction({
+            main_account, 100).buildTransaction({
             'chainId': 56,
             'gas': 180000,
             'gasPrice': web3.toWei(gas_price, 'gwei'),
@@ -51,9 +51,11 @@ def claim_mint_reward_and_share(account, contract):
 
 if (__name__ == '__main__'):
     accounts = parse_accounts(file_accounts)
+    web3 = Web3(Web3.HTTPProvider(web3_http_provider))
     with open(file_abi, 'r') as file:
         abi = file.read().replace('\n', '')
-    web3 = Web3(Web3.HTTPProvider(web3_http_provider))
+    with open(file_config, 'r') as file:
+        main_account = web3.toChecksumAddress(file.read().replace('\n', ''))
     contract = web3.eth.contract(address=web3.toChecksumAddress(contract_address), abi=abi)
     for account in accounts:
-        Thread(target=claim_mint_reward_and_share, args=(account, contract)).start()
+        Thread(target=claim_mint_reward_and_share, args=(account, contract, main_account)).start()
